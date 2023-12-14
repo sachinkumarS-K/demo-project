45df70d92c7d2b35fa5e9e28bf22de2742afe693
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2023-12-14 14:37:26 +0530

    modification done

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 require("dotenv").config();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`server is listining on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`server is listining on port ${PORT}`);
 });
